36b05b01355bca6e115f7e67639810917fd69845
lann95081 <lann95081@gmail.com>
2023-03-08 13:20:38 +0700

    up

--- case-study/src/app/app.module.ts
+++ case-study/src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { FacilityComponent } from './component/facility/facility.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { ContractComponent } from './component/contract/contract.component';
+import { FacilityListComponent } from './component/facility/facility-list/facility-list.component';
+import { FacilityEditComponent } from './component/facility/facility-edit/facility-edit.component';
+import { CreateComponent } from './component/facility/create/create.component';
+import { FacilityCreateComponent } from './component/facility/facility-create/facility-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FacilityComponent,
+    CustomerComponent,
+    ContractComponent,
+    FacilityListComponent,
+    FacilityEditComponent,
+    CreateComponent,
+    FacilityCreateComponent
   ],
   imports: [
     BrowserModule,
