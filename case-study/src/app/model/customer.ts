5b087dcbe12621776ada07050e71023bed726a2a
lann95081 <lann95081@gmail.com>
2023-03-12 13:58:13 +0700

    up

---
+++ case-study/src/app/model/customer.ts
@@ -0,0 +1,13 @@
+import { CustomerType } from './customer-type';
+
+export interface Customer {
+  id?: number;
+  name?: string;
+  gender?: number;
+  dayOfBirth?: string;
+  idCard?: string;
+  phoneNumber?: string;
+  email?: string;
+  address?: string;
+  customerType?: CustomerType;
+}
